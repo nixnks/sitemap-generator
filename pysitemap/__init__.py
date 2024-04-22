1035be7c8665eeb2ec72ba0565ab3e349214462c
Nix <nikusunix@gmail.com>
2024-04-22 22:07:54 +0300

    ref: grouped on steps, add typing and argument, add ValueError except. Formatted

--- pysitemap/__init__.py
+++ pysitemap/__init__.py
@@ -1,42 +1,52 @@
 import asyncio
 import signal
-from pysitemap.base_crawler import Crawler
 import logging
+from pysitemap.base_crawler import Crawler
+
 logger = logging.getLogger(__name__)
 
 
 def crawler(
-    root_url, out_file, out_format='xml', maxtasks=100,
-    exclude_urls=None, http_request_options=None,
-    parser=None
+        root_url: str,
+        out_file: str = None,
+        out_file_format: str = 'xml',
+        max_workers=64,
+        exclude_urls=None,
+        http_request_options=None,
+        parser=None,
+        verbose: bool = False
 ):
-    """
-    run crowler
-    :param root_url: Site root url
-    :param out_file: path to the out file
-    :param out_format: format of out file [xml, txt]
-    :param maxtasks: max count of tasks
-    :return:
-    """
-    loop = asyncio.get_event_loop()
-    c = Crawler(
-        root_url, out_file=out_file, out_format=out_format, maxtasks=maxtasks, http_request_options=http_request_options
-    )
-    
-    if parser is not None:
-        c.set_parser(parser_class=parser)
+    def _setup_logger():
+        if verbose:
+            logging.basicConfig(level=logging.DEBUG)
+        else:
+            logging.basicConfig(level=logging.INFO)
 
-    if exclude_urls:
-        c.set_exclude_url(urls_list=exclude_urls)
+    def _run_crawler():
+        loop = asyncio.get_event_loop()
+        c = Crawler(
+            root_url, out_file=out_file, out_format=out_file_format, maxtasks=max_workers,
+            http_request_options=http_request_options
+        )
 
-    loop.run_until_complete(c.run())
+        if parser is not None:
+            c.set_parser(parser_class=parser)
 
-    try:
-        loop.add_signal_handler(signal.SIGINT, loop.stop)
-    except (RuntimeError, ValueError):
-        '''Except ValueError: signal only works in main thread'''
-        pass
-    logger.info('todo_queue:', len(c.todo_queue))
-    logger.info('busy:', len(c.busy))
-    logger.info('done:', len(c.done), '; ok:', sum(c.done.values()))
-    logger.info('tasks:', len(c.tasks))
+        if exclude_urls:
+            c.set_exclude_url(urls_list=exclude_urls)
+
+        loop.run_until_complete(c.run())
+
+        try:
+            loop.add_signal_handler(signal.SIGINT, loop.stop)
+        except (RuntimeError, ValueError):
+            '''Except ValueError: signal only works in main thread'''
+            pass
+
+        logger.debug(f'Queue: {len(c.todo_queue)}\n'
+                     f'Busy: {len(c.busy)}\n'
+                     f'Done: {len(c.done)}\n'
+                     f'Tasks: {len(c.tasks)}\n')
+
+    _setup_logger()
+    _run_crawler()
